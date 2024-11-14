4203308c0a0d2f45e3a7f038a94454f1ca6035d1
German <germants100@gmail.com>
2024-11-13 21:03:42 -0600

    creating GET

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,17 +1,31 @@
 import { connectDB } from "@/libs/mongoDB";
 import User from "@/models/user"
-import { NextResponse } from "next/server";
+import { time } from "console";
+import { NextResponse, NextRequest } from "next/server";
+import path from "path";
 
-export async function GET() {
-    await connectDB();
-    const users = await User.find();
-    return NextResponse.json(users);
-}
 
-export async function POST(request: Request) {
-    await connectDB();
-    const data = await request.json();
+export async function GET (request:NextRequest){
+    try{
+        await connectDB();
+        const url = new URL(request.url);
+        const id = url.searchParams.get("id");
+        if(!id){
+            const users = await User.find();
+            return NextResponse.json( 
+                {
+                    succes:true,
+                    status: 200,
+                    message: "Users found",
+                    data: users,
+                    count: users.length,
+                    timestamp: new Date().getTime(),
+                    path: "api/users",
+                    method: "GET"
+                }, {status: 200}
+            )
+        }
+    } catch(error){
 
-    const user = await User.create(data);
-    return NextResponse.json(user);
-}
+    }
+ }
